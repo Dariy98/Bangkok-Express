2a1fd055904dcd24c6a9ba296f9bd6efa91b930b
Dariy98 <ostapenko.d98@gmail.com>
2020-07-30 23:52:49 +0300

    connect slider with cart

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,9 +1,12 @@
 import React, { useRef } from "react";
 import Swiper from 'react-id-swiper';
 
+import { useProducts } from '../context';
+
 export default function Slider () {
     const ref = useRef(null);
-
+    const { cards, collectArrCart } = useProducts()
+    
     const goNext = () => {
       if (ref.current !== null && ref.current.swiper !== null) {
         ref.current.swiper.slideNext();
@@ -15,11 +18,16 @@ export default function Slider () {
         ref.current.swiper.slidePrev();
       }
     };
-  
+    
+    const findDish = (dish) => {
+        const card = cards.find(card => card.name === dish)
+        collectArrCart(card)
+    }
+
     return (
   <div className="carousel">
         <Swiper ref={ref}>
-
+            
             <div>
                 <div className="carousel__slide">
                     <img
@@ -30,7 +38,7 @@ export default function Slider () {
                     <div className="carousel__caption">
                         <span className="carousel__price">€16.00</span>
                         <div className="carousel__title">Penang shrimp</div>
-                        <button type="button" className="carousel__button">
+                        <button type="button" className="carousel__button" onClick={() => findDish("Penang shrimp")}>
                             <img  src={require('../icons/plus-icon.svg')} alt="icon" />
                         </button>
                     </div>
@@ -46,8 +54,8 @@ export default function Slider () {
                         />
                     <div className="carousel__caption">
                         <span className="carousel__price">€14.00</span>
-                    <div className="carousel__title">Penang shrimp</div>
-                        <button type="button" className="carousel__button">
+                    <div className="carousel__title">Chicken cashew</div>
+                        <button type="button" className="carousel__button" onClick={() => findDish("Chicken cashew")}>
                             <img  src={require('../icons/plus-icon.svg')} alt="icon" />
                         </button>
                     </div>
@@ -64,7 +72,7 @@ export default function Slider () {
                     <div className="carousel__caption">
                         <span className="carousel__price">€12.50</span>
                         <div className="carousel__title">Red curry veggies</div>
-                        <button type="button" className="carousel__button">
+                        <button type="button" className="carousel__button" onClick={() => findDish("Red curry veggies")}>
                         <img  src={require('../icons/plus-icon.svg')} alt="icon" />
                         </button>
                     </div>
@@ -81,7 +89,7 @@ export default function Slider () {
                     <div className="carousel__caption">
                         <span className="carousel__price">€6.50</span>
                         <div className="carousel__title">Chicken springrolls</div>
-                        <button type="button" className="carousel__button">
+                        <button type="button" className="carousel__button" onClick={() => findDish("Chicken springrolls")}>
                             <img  src={require('../icons/plus-icon.svg')} alt="icon" />
                         </button>
                     </div>
